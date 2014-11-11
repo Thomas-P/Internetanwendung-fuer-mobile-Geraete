ae3f9e3dd340d4313206bacf4f9e2baf240dcf89
ThomasP <ThomasP.mail@gmx.de>
2014-11-11 14:03:28 +0100

    JSR2 with render container and refactored viewController.js

--- Projektordner/js/viewController.js
+++ Projektordner/js/viewController.js
@@ -6,34 +6,13 @@
 */
 var viewController = function() {
 
-	var wrapperElement = document.createElement('div');
-	wrapperElement.classList.add('wrapper');
 
-	var medienweiseElement;
 
-	var actualWrapper;
+
+
 	/**
-	*	get a new element of medienweise
+	*	JSR2 - 4.
 	*/
-	var createMedienweiseElement = function(secondTime) {
-		if (medienweiseElement)
-			return medienweiseElement.cloneNode(true);
-		if (secondTime === true) {
-			console.log('could not wrap medienweise');
-			return null;
-		}
-		medienweiseElement = document.querySelector('.medienweise');
-		if (medienweiseElement) {
-			var list = medienweiseElement.querySelector('ul');
-			while (list.children.length)
-				list.removeChild(list.children[0]);
-		}
-		return createMedienweiseElement(true);
-	};
-	// end function medienweise
-
-
-
 	var kringelKlick = function(event) {
 		//do a kringelKlick
 		if (!event || !event.target || event.target.id!='objektKringel')
@@ -51,87 +30,151 @@ var viewController = function() {
 
 
 
-	/**
-	*	generate a text view by the given content
-	*/
-	var createTextauzug = function(text) {
-		var TextauszugElement = document.getElementById('textauszug');
-		if (!TextauszugElement)
-			return Error('Textauszug element not found');
-		var szeneElement = document.querySelector('#textauszug .szene');
-		if (!szeneElement)
-			return Error('Could not found element for scene');
-		// bad idea -> message to tutor written
-		szeneElement.innerHTML = text;
-		TextauszugElement.style.display = 'inline-block';
-	};
-	// end createTextauszug
-	/**
-	*	Zeitdokumente
-	*/
-	var createObject = function(objectData) {
 
-		var objectElement = document.querySelector('#objekt');
-		if (!objectElement)
-			return Error('Objekt element not found');
 
-		var image = objectElement.querySelector('img');
-		image.dataset.backupUrl = objectData.src_bak || void 0;
-		image.alt = objectData.title || 'an image';
-		image.src = objectData.src;
-		image.dataset.description = objectData.description || 'Keine Beschreibung angegeben.';
 
-		var figcaption = objectElement.querySelector('figcaption');
-		if (!figcaption)
-			return;
-		figcaption.textContent = objectData.title || '';
-		objectElement.style.display = 'inline-block';
-	};
-	// end createObject
 	/**
-	*	
+	*	JSR2 - 3
 	*/
-	var createMedienweise = function(medienObject) {
-		var element = createMedienweiseElement();
+	var createTextauszug = function(object) {
+		var element = getElements(object.type);
 		if (!element) {
-			console.log('Could not create a element of medienweise.');
+			console.error('Could\'nt find textauszug element.');
 			return;
 		}
-		element.id = medienObject._contentitemid || Math.round(Math.random()*(1<<23));
+		/**
+		*	generate a text view by the given content
+		*/
+		var createTextauzugText = function(text) {
+			var szeneElement = element.querySelector('.szene');
+			if (!szeneElement)
+				return Error('Could not found element for scene');
+			// bad idea -> message to tutor written
+			szeneElement.innerHTML = text;
+			// place to container
+			placeElement(element,object.render_container);
+		};
+
+		xhr(object.src || null)
+		.then(createTextauzugText)
+		.catch(function(error) {
+			console.error('Error while creating Textauszug: ',error);
+		});
+	};
+	// end createTextauszug
+
+
+
+
+
+	/**
+	*	JSR2 - 4 Zeitdokumente
+	*/
+	var createZeitdokumente = function(object) {
+		var element = getElements(object.type);
+		if (!element) {
+			console.error('Could\'nt find "Zeitdokumente" element.');
+			return;
+		}
+
+		var image = element.querySelector('img');
+		image.dataset.backupUrl = object.src_bak || void 0;
+		image.alt = object.title || 'an image';
+		image.src = object.src;
+		image.dataset.description = object.description || 'Keine Beschreibung angegeben.';
+
+		var figcaption = element.querySelector('figcaption');
+		if (!figcaption)
+			return;
+		figcaption.textContent = object.title || '';
+		// place to container
+		placeElement(element,object.render_container);
+	};
+	// end createZeitdokumente
+
+
+
+
+
+	/**
+	*	create medienverweis or verknuepfung
+	*/
+	var createMedienVerweise = function(object) {
+		var element = getElements(object.type);
+		if (!element) {
+			console.error('Could not create a element of ',object.type,'.');
+			return;
+		}
+		element.id = object._contentitemid || Math.round(Math.random()*(1<<23));
 
 		var title  = element.querySelector('section > header h2');
 		if (title)
-			title.textContent = medienObject.subtype;
+			title.textContent = object.subtype || object.title || 'No Title';
 
 		var list = element.querySelector('ul');
 		if (!list)
 			return;
 
-		if (!medienObject || !medienObject.content || !Array.isArray(medienObject.content)) 
+		if (!object || !object.content || !Array.isArray(object.content)) 
 			return;
 
-		medienObject.content.forEach(function(el) {
+		object.content.forEach(function(el) {
 
 			var listElement = document.createElement('li');
 
 			var a = document.createElement('a');
-			a.href = el.target || '#';
+			a.href = el.target || el.src || '#';
 			a.textContent = el.title;
+			if (el.mediatype) {
+				a.type = el.mediatype;
+			}
 
 			// append
 			listElement.appendChild(a);
 			list.appendChild(listElement);
 		});
-
-		if (!actualWrapper || actualWrapper.children.length>1) {
-			actualWrapper = wrapperElement.cloneNode(true);
-			var area = document.querySelector('#mainScrollArea');
-			if (area)
-				area.appendChild(actualWrapper);
-		}
-		if (actualWrapper)
-			actualWrapper.appendChild(element);
+		// placing to the render container
+		placeElement(element,object.render_container);
 	};
+	// end createMedienVerweise
+
+
+
+
+
+	/**
+	*	create all the items in the given list
+	*/
+	var createContentItems = function(items) {
+		if (!items)
+			return;
+		items.forEach(function(element) {
+			if (!element.type)
+				return;
+			switch (element.type) {
+				case 'zeitdokumente':
+				createZeitdokumente(element);
+				break;
+				// create a textauszug
+				case 'textauszug':
+				createTextauszug(element);
+				break;
+				// create object
+				case 'object': 
+
+				break;
+				// create medienweise
+				case 'medienverweise':
+				createMedienVerweise(element);
+				break;
+			}
+		});
+	};
+
+
+
+
+
 	/**
 	*	create the view by the given view data;
 	*/
@@ -147,55 +190,177 @@ var viewController = function() {
 			return viewData.content_items;
 	};
 
-	var createContentItems = function(items) {
-		if (!items)
-			return;
-		items.forEach(function(element) {
-			if (!element.type)
-				return;
-			switch (element.type) {
-				case 'zeitdokumente':
-				createObject(element);
-				break;
-				// create a textauszug
-				case 'textauszug':
-				xhr(element.src || null)
-				.then(createTextauzug)
-				.catch(function(error) {
-					console.error('Error while creating Textauszug: ',error);
-				});
-				break;
-				// create object
-				case 'object': 
 
-				break;
-				// create medienweise
-				case 'medienverweise':
-				createMedienweise(element);
-				break;
-			}
-		});
+
+
+
+	/**
+	*
+	*/
+	var getElements = function(elementName) {
+		var tmp = View.templates;
+		if (!tmp[elementName]) {
+			console.error('Could\'nt find an element with ',elementName,' in template list');
+			return null;
+		}
+		// return an element from templates node
+		if (elementName == 'medienverweise' || elementName == 'verknuepfungen') {
+			return tmp[elementName].cloneNode(true);
+		} else {
+			return tmp[elementName];
+		}
 	};
+
+
+
+
+
+
+	/**
+	*	create a new wrapper element for medienweise objects
+	*/
+	var createWrapper = function() {
+		var wrapperElement = document.createElement('div');
+		wrapperElement.classList.add('wrapper');
+		return wrapperElement;
+	};
+
+
+
+
+
+
+	/**
+	*	places an element to the right container
+	*/
+	var placeElement = function(element,renderContainer) {
+		var tmpRC = View.renderContainer || {};
+		
+		if (!element)
+			return;
+		if (!tmpRC[renderContainer]) {
+			console.error('Render container not found: ',renderContainer);
+			return;
+		}
+
+		tmpRC = tmpRC[renderContainer];
+
+		if (element.classList.contains('medienweise')) {
+			var actualWrapper = tmpRC.actualWrapper;
+			if (!actualWrapper || actualWrapper.children.length>1) {
+				// generate a new wrapper
+				actualWrapper = createWrapper();
+				tmpRC.actualWrapper = actualWrapper;
+				tmpRC.domElement.appendChild(actualWrapper);
+			}
+			if (actualWrapper)
+				actualWrapper.appendChild(element);
+			return;
+		}
+
+		tmpRC.domElement.appendChild(element);
+
+	};
+
+
+
+
+
+
+
+	var isFirst = true;
+	// view is the mainframe for our view
+	var View = {};
+
+	var firstStart = function() {
+
+		if (!isFirst)
+			return;
+		isFirst = false;
+
+		var tmpC = View.container = document.querySelector('.main-view');
+
+		View.templates = {
+			textauszug : document.querySelector('#textauszug') || document.createElement('div'),
+			medienverweise: document.querySelector('.medienweise') || document.createElement('div'),
+			zeitdokumente: document.querySelector('#objekt') || document.createElement('div'),
+			footer: document.querySelector('footer.wrapper') || document.createElement('div')
+		};
+
+		// remove list entries from medienverweise
+		var tmpM = View.templates.medienverweise.querySelector('ul');
+		if (tmpM) {
+			while (tmpM.children.length)
+				tmpM.removeChild(tmpM.children[0]);
+		}
+
+		// create Render Container
+		var tmpRC = View.renderContainer = {};
+
+		// get name for render container
+		if (tmpC && tmpC.dataset.container) {
+			var tmpSplit = View.renderOrder = tmpC.dataset.container.split(',');
+
+			// create for each named container one dom element and a wrapper node
+			tmpSplit.forEach(function(cName) {
+				var tmpDomElement = document.createElement('div');
+				tmpDomElement.id = cName;
+				tmpDomElement.classList.add('render-container');
+				// add a new container with value of cName 
+				tmpRC[cName] = {
+					domElement: tmpDomElement,
+					actualWrapper: null
+				};
+			});
+		}
+	};
+
+
+
+
+
+
+	/**
+	*	clear renderview
+	*/
+	var clear = function() {
+		// get template fragments on first start
+		firstStart();
+		if (View.container) {
+			// clearing view container
+			while (View.container.children.length)
+				View.container.removeChild(View.container.children[0]);
+		}
+		// clear wrapper
+		var tmpRC = View.renderContainer || {};
+		for (var key in tmpRC) {
+			if (tmpRC[key].actualWrapper)
+				tmpRC[key].actualWrapper = null;
+			// temp for dom elements of render container
+			var tmpDE = tmpRC[key].domElement;
+			while(tmpDE.children.length) {
+				tmpDE.removeChild(tmpDE.children[0]);
+			}
+		}
+		if (View.renderOrder) {
+			// place render container in the right order
+			View.renderOrder.forEach(function(cName) {
+				View.container.appendChild(tmpRC[cName].domElement);
+			});
+		}
+	};
+
+
+
+
+
+
 
 	// Closure function 
 	return function (viewLink) {
-		// create view by given config via viewLink
-		var contentElement = document.querySelector('#mainScrollArea');
-		if (contentElement) {
-			doUnvisible = ['#objekt', '#einfuehrungstext', '#textauszug'];
-			var element;
-			doUnvisible.forEach(function(id) {
-				element = contentElement.querySelector(id);
-				if (!element)
-					return;
-				element.style.display = 'none';
-			});
-			if (!medienweiseElement)
-				createMedienweiseElement();
-			while (element = contentElement.querySelector('div.wrapper')) {
-				contentElement.removeChild(element);
-			};
-		}
+		// clear View;
+		clear();
+		
 		xhr(viewLink)
 		.then(createView)
 		.then(createContentItems)
